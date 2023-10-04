5b9c7f865b35d24193a3ac8a296d4ef48ef4231e
smedtr <smedtr@email.com>
2023-10-04 17:52:07 +0200

    File Upload incorrect url

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    path("graphql", csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
 ]
+
+urlpatterns = urlpatterns + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
